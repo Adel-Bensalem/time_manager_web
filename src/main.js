13b56c4c5182d4665667690f05e7baaa2edab5ad
Bensalem Adel <vild@debian>
2020-10-26 10:06:42 +0100

    move the ui specific code to a ui directory

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import App from './ui/App.vue'
+import router from './ui/router'
 
 createApp(App).use(router).mount('#app')
