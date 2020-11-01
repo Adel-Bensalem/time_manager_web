2e3c611bee6b78cd3ae6d1927e7beead3ff002ba
adel.bensalem1 <adel.bensalem@augeo.org>
2020-11-01 22:07:52 +0100

    add main module

--- src/main.js
+++ src/main.js
@@ -1,5 +1,33 @@
 import { createApp } from 'vue'
+
+import { createCore } from "./core/main";
+import { createServices } from "./services/main";
+import { createState, createMessageDispatcher, createSelector } from "./state/main";
+import { sendRequest } from "./tools/sendRequest";
+import { createDispatcherToPresenterAdapter } from "./tools/dispatcherToPresenterAdapter";
+
 import App from './ui/App.vue'
 import router from './ui/router'
 
-createApp(App).use(router).mount('#app')
+const services = createServices(sendRequest);
+const state = createState();
+const messageDispatcher = createMessageDispatcher(state.dispatch);
+const selector = createSelector(state.getState);
+const core = createCore(
+    services,
+    createDispatcherToPresenterAdapter(messageDispatcher)
+);
+
+createApp(App)
+    .use((app) => {
+        app.mixin({
+            beforeCreate() {
+                this.core = core;
+                this.messageDispatcher = messageDispatcher;
+                this.selector = selector;
+                this.subscribeToStore = (callSubscriber) => state.subscribe(() => callSubscriber());
+            }
+        });
+    })
+    .use(router)
+    .mount('#app');
