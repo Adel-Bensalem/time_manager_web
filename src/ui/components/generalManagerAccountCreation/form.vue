e1019dd9d73f51aba0ff1a58403d94276f9c746b
Bensalem Adel <vild@debian>
2020-11-01 14:06:51 +0100

    add the general manager account creation view

---
+++ src/ui/components/generalManagerAccountCreation/form.vue
@@ -0,0 +1,68 @@
+<template>
+    <div class="form">
+        <input
+                type="text"
+                class="form__input"
+                placeholder="Email"
+                :value="data.email"
+                @input="editForm({ ...data, email: $event.data })"
+        />
+        <input
+                type="text"
+                class="form__input"
+                placeholder="Full name"
+                :value="data.fullName"
+                @input="editForm({ ...data, fullName: $event.data })"
+        />
+        <input
+                type="password"
+                class="form__input"
+                placeholder="Password"
+                :value="data.password"
+                @input="editForm({ ...data, password: $event.data })"
+        />
+        <input
+                type="password"
+                class="form__input"
+                placeholder="Password confirmation"
+                :value="data.passwordConfirmation"
+                @input="editForm({ ...data, passwordConfirmation: $event.data })"
+        />
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Form",
+        props: {
+            data: {
+                email: String,
+                fullName: String,
+                password: String,
+                passwordConfirmation: String
+            }
+        },
+        methods: {
+            editForm(nextData) {
+                console.log(nextData)
+                this.$emit("edit-form", nextData);
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .form {
+        display: flex;
+        flex-direction: column;
+    }
+
+    .form__input {
+        flex: 1;
+        font-size: 1.4rem;
+    }
+
+    .form__input:not(:last-child) {
+        margin-bottom: 1rem;
+    }
+</style>
---
+++ src/ui/components/generalManagerAccountCreation/index.vue
@@ -0,0 +1,57 @@
+<template>
+    <div class="account-creation">
+        <div class="account-creation__form">
+            <account-creation-form :data="formData" @edit-form="editGeneralManagerAccountCreationForm" />
+        </div>
+        <button class="account-creation__button" @click="createGeneralManagerAccount">Send</button>
+    </div>
+</template>
+
+<script>
+    import Form from "./form";
+
+    export default {
+        name: "GeneralManagerAccountCreation",
+        props: {
+            formData: {
+                email: String,
+                fullName: String,
+                password: String,
+                passwordConfirmation: String
+            }
+        },
+        components: { 'account-creation-form': Form },
+        methods: {
+            editGeneralManagerAccountCreationForm(formData) {
+                this.$emit("edit-general-manager-account-creation-form", formData);
+            },
+            createGeneralManagerAccount() {
+                this.$emit("create-general-manager-account", this.formData);
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .account-creation {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+
+    .account-creation__form {
+        max-width: 40rem;
+        width: 100%;
+        margin-bottom: 2rem;
+    }
+
+    .account-creation__button {
+        border: none;
+        border-radius: 5px;
+        color: #fafafa;
+        padding: 6px 2rem;
+        font-size: 1.4rem;
+        background-image: linear-gradient(to right bottom, #42b983, #2e9567);
+        cursor: pointer;
+    }
+</style>
