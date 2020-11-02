7e88fac410405f9dc8a97c1b89c18e2ee5c4d7af
adel.bensalem1 <adel1.bensalem@epitech.eu>
2020-11-02 19:47:14 +0100

    update api mock

--- server/api/index.js
+++ server/api/index.js
@@ -1,9 +1,20 @@
 const router = require("express").Router();
 
-router.get(
+router.post(
     "/authenticate",
     (req, res) => res.status(200).send({
         token: "This is a fake token, it's useless"
+    }));
+
+router.get(
+    "/authenticate",
+    (req, res) => res.status(200).send({
+        account: { fullName: "John Doe", email: "john.doe@test.test" },
+        role: {
+            isGeneralManager: true,
+            isManager: false,
+            isEmployee: false
+        },
     }));
 
 module.exports = router;
