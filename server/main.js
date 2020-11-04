29b148381ef40fec78e3812d1af04c99d15dce78
adel.bensalem1 <adel1.bensalem@epitech.eu>
2020-11-04 20:10:12 +0100

    fix server

--- server/main.js
+++ server/main.js
@@ -1,18 +1,19 @@
 const express = require('express');
+const { resolve } = require('path');
 const history = require('connect-history-api-fallback');
 const proxy = require("express-http-proxy");
 
 const apiMockRouter = require("./api");
 const app = express();
-const staticFileMiddleware = express.static('dist');
 
-app.use(staticFileMiddleware);
 app.use("/api", process.env.NODE_ENV === "production" ? proxy(process.env.API_URL) : apiMockRouter);
 
 app.use(history({
     index: '/dist/index.html'
 }));
 
-app.use(staticFileMiddleware);
+app.use(express.static(resolve(__dirname, '..', 'dist')));
+
+app.use('*', (req, res) => res.sendFile(resolve(__dirname, '..', 'dist', 'index.html')));
 
 app.listen(3000, () => console.log('App listening on port 3000'));
