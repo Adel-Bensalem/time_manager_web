effbe86fccc061bc133d9fee053e127ffa57f1e5
adel.bensalem1 <adel1.bensalem@epitech.eu>
2020-11-05 20:52:43 +0100

    refactor sendService

--- src/tools/httpClient.js
+++ src/tools/httpClient.js
@@ -1,16 +1,11 @@
 import axios from "axios";
 
-function sendRequest(endpoint, method, data) {
-    const requestData = method === "GET" ? {params: data} : {data};
-
+function sendRequest(config) {
     return new Promise((resolve, reject) =>
             axios({
-                url: endpoint,
-                method,
-                headers: {
-                    "Content-Type": "application/json"
-                },
-                ...requestData
+                url: config.endpoint,
+                headers: { "Content-Type": "application/json", ...config.headers },
+                ...config
             })
                 .then(({data}) => resolve(data))
                 .catch(error =>
